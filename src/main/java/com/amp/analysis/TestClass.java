cade381796420195da6172872f144e9ffc40441a
unknown <krlu91@gmail.com>
2019-08-24 16:04:44 -0400

    initial scanning and printing of test java program

---
+++ src/main/java/com/amp/analysis/TestClass.java
@@ -0,0 +1,9 @@
+package com.amp.analysis;
+import spoon.Launcher;
+
+public class TestClass {
+    public double testMethod() {
+        Launcher launcher = new Launcher();
+        return 0.0;
+    }
+}
